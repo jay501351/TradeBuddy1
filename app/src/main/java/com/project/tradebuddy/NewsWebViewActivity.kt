63cc40d5da932f48b8a7fb94e8ac0cb03b498d9d
jay501351 <jaylatiwala5505@gmail.com>
2025-06-18 15:10:55 +0530

    toolbar added

--- app/src/main/java/com/project/tradebuddy/NewsWebViewActivity.kt
+++ app/src/main/java/com/project/tradebuddy/NewsWebViewActivity.kt
@@ -5,12 +5,21 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class NewsWebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_news_webview)
+
+        val toolbar = findViewById<Toolbar>(R.id.newsToolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
 
         val webView = findViewById<WebView>(R.id.webView)
         val url = intent.getStringExtra("url")
